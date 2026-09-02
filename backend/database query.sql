bff22ac772ffc9839f9a967e38b8535fa6fbbe60
Junyang Feng <jfen0074@student.monash.edu>
2026-09-02 12:52:47 +1000

    Update database query.sql

--- backend/database query.sql
+++ backend/database query.sql
@@ -1,4 +1,4 @@
-# 有什么table
+# have what table
 
 SELECT c.relname AS table_name,
        CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' END AS kind,
@@ -11,12 +11,12 @@ ORDER BY pg_total_relation_size(c.oid) DESC;
 
 
 
-#某个表有什么列
+# check table schema
 SELECT column_name, data_type, is_nullable
 FROM information_schema.columns
 WHERE table_schema = 'public' AND table_name = 'urban_tree'
 ORDER BY ordinal_position;
 
-#看前几行
+# check first 5 row
 SELECT * FROM urban_tree LIMIT 5;
 
